f90e95c494cded2d5d1537a98d747544ebb9c7a9
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2022-09-14 17:01:07 -0500

    componente ingresos -55

--- src/app/ingreso/ingreso.component.ts
+++ src/app/ingreso/ingreso.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingreso } from './ingreso.model';
+import { IngresoServicio } from './ingreso.servicio';
 
 @Component({
   selector: 'app-ingreso',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingreso.component.css']
 })
 export class IngresoComponent implements OnInit {
+  //necesitamos acceder al arreglo de cada uno de los elementos del arreglo de ingresos
+  ingresos:Ingreso[]=[];
 
-  constructor() { }
+  //inyectamos una instacia
+  constructor(private ingresoServicio:IngresoServicio) { }
 
   ngOnInit(): void {
+    this.ingresos = this.ingresoServicio.ingresos;
+  }
+
+  eliminarRegistro(ingreso:Ingreso){
+    this.ingresoServicio.eliminar(ingreso); //eliminar pasandole la referencia del ingreso que se haya seleccionado para eliminar
   }
 
 }
