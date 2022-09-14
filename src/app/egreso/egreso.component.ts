d1e1a8611026243711efc0b74f4fd3d7b6dc17e5
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2022-09-14 18:13:23 -0500

    componente egresos -56

--- src/app/egreso/egreso.component.ts
+++ src/app/egreso/egreso.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Egreso } from './egreso.model'; //importamos la clase de Egreso
+import { EgresoServicio } from './egreso.servicio';
 
 @Component({
   selector: 'app-egreso',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EgresoComponent implements OnInit {
 
-  constructor() { }
+  @Input() ingresoTotal:number; //importamos este método, para calular los porcentajes
+  //necesitamos acceder al arreglo de cada uno de los arreglos de tipo egresos
+  egresos:Egreso[]=[]; 
+
+  //inyectamos una instancia
+  constructor(private egresoServicio:EgresoServicio) { }
 
   ngOnInit(): void {
+    this.egresos = this.egresoServicio.egreso;
+  }
+
+  //en la clase egresoServicio se encuentra este método
+  eliminarRegistro(egreso:Egreso){
+    this.egresoServicio.eliminar(egreso);
+  }
+  calcularPorcentaje(egreso:Egreso){
+    return egreso.valor/this.ingresoTotal;
   }
 
 }
--- src/app/egreso/egreso.servicio.ts
+++ src/app/egreso/egreso.servicio.ts
@@ -5,4 +5,9 @@ export class EgresoServicio{
         new Egreso("Renta Depto", 900),
         new Egreso("Ropa", 200)
     ];
+
+    eliminar(egreso:Egreso){
+        const indice: number = this.egreso.indexOf(egreso);
+        this.egreso.splice(indice,1);
+    }
 }
